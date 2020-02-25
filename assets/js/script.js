c7c7796df79f149f4aeb5a30ebe6e446b4d5d1ac
ellenlowing <lowing@bu.edu>
2020-02-25 14:10:49 -0500

    Fix scaling of iframe, mobile nav centered kind of

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 window.onload = function() {
 
   let zoom = () => {
-    let f = window.innerWidth > 600 ? 0.000638 : 0.000594;
-    let scl = f * window.innerWidth;
-    $('iframe').css({
-      '-o-transform': `scale(${scl}`,
-      '-webkit-transform': `scale(${scl}`,
-      '-moz-transform': `scale(${scl}`,
-      '-ms-zoom': scl,
-      'margin-bottom': `-${(1-scl) * 900}px`
-    });
+    if(window.innerWidth < 1440) {
+      let f = window.innerWidth > 600 ? 0.000638 : 0.000594;
+      let scl = f * window.innerWidth;
+      $('iframe').css({
+        '-o-transform': `scale(${scl}`,
+        '-webkit-transform': `scale(${scl}`,
+        '-moz-transform': `scale(${scl}`,
+        '-ms-zoom': `${scl}`,
+        'margin-bottom': `-${(1-scl) * 900}px`
+      });
+    } else {
+      let f = window.innerWidth / 1440 * 0.92;
+      $('iframe').css({
+        '-o-transform': `scale(${f})`,
+        '-webkit-transform': `scale(${f})`,
+        '-moz-transform': `scale(${f})`,
+        '-ms-zoom': `${f}`,
+        'margin-bottom': `${(f-1) * 900}px`
+      });
+    }
   }
   zoom();
   $( window ).resize(zoom);
@@ -20,7 +31,9 @@ window.onload = function() {
       e.preventDefault();
       $('body').removeClass('info-active');
       $('.work-active').removeClass('work-active');
+      $('.btn-active').removeClass('btn-active');
       $(`#${e.target.innerText}`).addClass('work-active');
+      $(this).addClass('btn-active');
       let elem = document.getElementById(`${e.target.innerText}`);
       if($('#theiframe').attr('src') !== elem.dataset.src) $('#theiframe').attr('src', elem.dataset.src);
     });
@@ -28,6 +41,7 @@ window.onload = function() {
 
   $('.info-btn').on('click', (e) => {
     e.preventDefault();
+    $('.btn-active').removeClass('btn-active');
     $('body').addClass('info-active');
     $('#theiframe').attr('src', '');
   });
